83269a3391a510df732ab0bef2f5c8920eb6880e
Vincenzo Minolfi <vincenzo.minolfi@gmail.com>
2025-08-08 10:18:21 +0200

    Updated copying script

--- build_and_dotfile_it.sh
+++ build_and_dotfile_it.sh
@@ -3,5 +3,6 @@
 # This script builds ratatoskr and then copies the exec to ~/.config/niri
 
 cargo build --release;
-cp ~/Repositories/ratatoskr/target/release/ratatoskr ~/.config/niri/;
-echo -e "\n\033[0;32m\033[1mratatoskr built and copied to ~/.config/niri\033[0m";
+cp ~/Repositories/ratatoskr/target/release/ratatoskr ~/.config/niri/ \
+    && echo -e "\n\033[0;32m\033[1mratatoskr built and copied to ~/.config/niri\033[0m" \
+    || echo -e "\n\033[0;31m\033[1mratatoskr copying failed\033[0m"
