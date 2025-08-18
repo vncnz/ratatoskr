6ff28ecfca7c9b0909e901bad2c584712b03daa1
Vincenzo Minolfi <vincenzo.minolfi@gmail.com>
2025-08-18 11:43:45 +0200

    Fix weather locality

--- src/sysutils.rs
+++ src/sysutils.rs
@@ -217,7 +217,7 @@ fn get_brightness () -> SysUpdate {
 */
 
 pub fn get_weather () -> WeatherStats {
-    let output = Command::new("/home/vncnz/.config/eww/scripts/meteo.sh").arg("'Desenzano Del Garda'").arg("45.457692").arg("10.570684").output();
+    let output = Command::new("/home/vncnz/.config/eww/scripts/meteo.sh").arg("Desenzano Del Garda").arg("45.457692").arg("10.570684").output();
     let stdout = String::from_utf8(output.unwrap().stdout).unwrap();
     // println!("\n{:?}", stdout);
     // let weather: WeatherObj;
